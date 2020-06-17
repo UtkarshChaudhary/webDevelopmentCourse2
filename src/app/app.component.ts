6ea14b94bf668a68250511ea4c3493631d12f5cd
Utkarsh Chaudhary <utkarshobyan@gmail.com>
2020-06-17 22:26:05 +0530

    components part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `<h1>{{title}}</h1>' inline template
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
