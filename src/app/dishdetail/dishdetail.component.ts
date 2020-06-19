bc9ef8cd8d44a4b1296763a02ae8b5759af9b6d8
Utkarsh Chaudhary <utkarshobyan@gmail.com>
2020-06-19 21:11:35 +0530

    RxJS Part2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,6 +6,7 @@ import {Params, ActivatedRoute} from '@angular/router';
 import { Location } from '@angular/common';
 //location is used to specify location of page in history of web browser
 import { DishService } from '../services/dish.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dishdetail',
@@ -16,15 +17,27 @@ export class DishdetailComponent implements OnInit {
   
   
   dish: Dish;
+  dishIds: string[];
+  prev: string;
+  next: string;
   
   constructor(private dishService: DishService, 
     private route: ActivatedRoute,
     private location: Location) { }
 
   ngOnInit() {
-    let id = this.route.snapshot.params['id'];
-    this.dishService.getDish(id)
-    .subscribe((dish) => this.dish = dish);
+    this.dishService.getDishIds().
+    subscribe((dishIds) => this.dishIds =  dishIds);
+
+    this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
+    .subscribe((dish) => {this.dish = dish; this.setPrevNext(dish.id); }); //now we get access of params observable
+    // here above params is observable by default, now switchmap function takes id value whenever observable changes and call getDish func i.e make new observable getDish()
+  }
+
+  setPrevNext(dishId: string){
+   const index = this.dishIds.indexOf(dishId);
+   this.prev = this.dishIds[(this.dishIds.length + index -1)%this.dishIds.length];
+   this.next = this.dishIds[(this.dishIds.length + index +1)%this.dishIds.length];
   }
 
   goBack(): void {
